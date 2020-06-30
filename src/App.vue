ffe64fb8bde894a3e486f58c621adc93b613d96b
Sylvain Langler <sylvain.langler@etu.unice.fr>
2020-06-30 22:47:32 +0200

    on met l'entrée du pseudo dans le composant principal pour mieux découper

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
 	<div id="app">
+		<div>
+			Entrez votre pseudo:
+		</div>
+		<div>
+			<input type="text" name="username" id="username" v-model="username">
+			<button v-on:click="updateUsername(username)">Valider</button>
+		</div>
 		<PokemonGame />
 	</div>
 </template>
 
 <script>
 import PokemonGame from "./components/PokemonGame.vue";
+import io from "socket.io-client";
 
 export default {
 	name: "App",
 	components: {
 		PokemonGame,
+	},
+	data() {
+		return {
+			username: '',
+			socket: {},
+			players: [],
+			ready: false,
+		};
+	}, 
+	created() {
+		// localhost:8101
+		// sylvainlangler.alwaysdata.net
+		this.socket = io("localhost:8101");
+	},
+	mounted() {
+		this.socket.on('updatePlayers', (data) => {
+			this.players = data;
+			console.log(this.players);
+		})
+	},
+	methods: {
+		updateUsername(username){
+			this.socket.emit('newPlayerUsername', {
+				name: username
+			});
+		}
 	},
 
 };
--- src/components/PokemonGame.vue
+++ src/components/PokemonGame.vue
@@ -1,46 +1,23 @@
 <template>
 	<div>
-		<div>
-			Entrez votre pseudo:
-		</div>
-		<div>
-			<input type="text" name="username" id="username" v-model="username">
-			<button v-on:click="updateUsername(username)">Valider</button>
-		</div>
+		le jeu
 	</div>
 </template>
 
 <script>
 
-import io from "socket.io-client";
-
 export default {
-	name: "CreateUser",
+	name: "PokemonGame",
 	data() {
 		return {
-			username: '',
-			socket: {},
-			players: [],
-			ready: false,
+
 		};
 	}, 
 	created() {
-		// localhost:8101
-		// sylvainlangler.alwaysdata.net
-		this.socket = io("sylvainlangler.alwaysdata.net");
 	},
 	mounted() {
-		this.socket.on('updatePlayers', (data) => {
-			this.players = data;
-			console.log(this.players);
-		})
 	},
 	methods: {
-		updateUsername(username){
-			this.socket.emit('newPlayerUsername', {
-				name: username
-			});
-		}
 	},
 };
 </script>
